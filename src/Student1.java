f88c555a20f632fcfb0041b36f7bab855b367db4
meghana <meghana.edu2020@gmail.com>
2023-10-25 17:28:40 -0500

    First Commit

---
+++ src/Student1.java
@@ -0,0 +1,12 @@
+
+public class Student1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 20;
+		if(x+y > 20) {    
+			System.out.println("x + y is greater than 20");    
+			}    
+	}
+}
